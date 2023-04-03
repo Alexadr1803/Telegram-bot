ff35fd01871da7bc79fa672ca727a70003a9101c
Alexadr1803 <97625955+Alexadr1803@users.noreply.github.com>
2023-04-03 19:02:50 +0300

    Add files via upload

--- Plutonium bot/main.py
+++ Plutonium bot/main.py
@@ -1,22 +1,25 @@
 import telebot
-from pathlib import Path
-import registration
-import sort_user
+import sqlite3
+from registration import register
+bot = telebot.TeleBot('6144683206:AAGEiDwpUkZRg25pnMym5YbkvlnycuT-k-I')
 
 
-def get_path():
-    raw_path = Path(__file__).resolve()
-    source_path = raw_path.parent
-    return str(source_path)
-
-
-token_path = Path(".tgtoken")
-bot = telebot.TeleBot(token_path.read_text().rstrip("\n\r"))
-
-
-@bot.message_handler()
-def get_text_messages(message):
-    sort_user.sort_users(message)
+@bot.message_handler(content_types=['text'])
+def sorting_users(msg):
+    con = sqlite3.connect("DataBase.db")
+    cur = con.cursor()
+    try:
+        res = list(cur.execute(f"SELECT place FROM history WHERE id={msg.chat.id}"))[0][0]
+        if res == 'reg_name':
+            # регистрация никнейма игрока
+            if len(msg.text) < 16:
+                cur.execute(f"INSERT INTO users(id, status, login) VALUES({msg.chat.id}, 'user', {msg.text})")
+                con.commit()
+                bot.send_message(msg.chat.id, "Вы зарегистрированы!")
+            else:
+                bot.send_message(msg.chat.id, "У вас слишком длинный ник! Введите новый!")
+    except IndexError:
+        register(bot, msg)
 
 
 bot.polling(none_stop=True, interval=0)
--- Plutonium bot/registration.py
+++ Plutonium bot/registration.py
@@ -1,2 +1,15 @@
-def reg():
-    print(1)
+import telebot
+import sqlite3
+
+
+def register(bot, msg):
+    con = sqlite3.connect("DataBase.db")
+    cur = con.cursor()
+    try:
+        result = list(cur.execute(f"SELECT login FROM users WHERE id={msg.chat.id}"))[0][0]
+
+    except IndexError:
+        bot.send_message(msg.chat.id, "Вам нужно зарегистрироваться!")
+        cur.execute(f"INSERT INTO history(id, place) VALUES({msg.chat.id}, 'reg_name')")
+        con.commit()
+        bot.send_message(msg.chat.id, "Напишите свой никнейм")
