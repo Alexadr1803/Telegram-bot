1db6bd9c3755a8bb809f9298007b691b66839bc5
Alexadr1803 <alexadr1803@gmail.com>
2023-04-05 21:41:46 +0300

    абоба

--- Plutonium bot/main.py
+++ Plutonium bot/main.py
@@ -20,17 +20,28 @@ def sorting_users(msg):
             keyboard = types.InlineKeyboardMarkup()
             callback_button = types.InlineKeyboardButton(text="Мой профиль", callback_data="profile")
             keyboard.add(callback_button)
-            bot.send_message(msg.chat.id, "Выберите действие из списка:", reply_markup=keyboard)
+            bot.send_message(msg.chat.id, "🤳Выберите действие из списка:", reply_markup=keyboard)
 
     except IndexError:
         register(bot, msg)
+
+
+@bot.message_handler(content_types=['sticker'])
+def anti_sticker(msg):
+    bot.send_message(msg.chat.id, "Каждый раз, когда вы отправляете мне стикер, в мире погибает 1 котенок(😿")
+
 
 @bot.callback_query_handler(func=lambda call: True)
 def callback_inline(call):
     # Если сообщение из чата с ботом
     if call.message:
         if call.data == "profile":
-            bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text="Пыщь")
+            role = {'user': "Гость", 'player': 'Игрок', "admin": "Админ", "moder": "Модератор"}
+            con = sqlite3.connect("DataBase.db")
+            cur = con.cursor()
+            result = list(cur.execute(f"SELECT login, status FROM users WHERE player_id={call.message.chat.id}"))[0]
+            bot.send_message(call.message.chat.id,
+                             f"Вот ваш профиль👤\n📝Ваш ник: {result[0]}\n♋Ваша роль: {role[result[1]]}")
 
 
 bot.polling(none_stop=True, interval=1)
